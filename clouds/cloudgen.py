bc4933342d0a0dd5e53d8293ff5c752a3707764c
Vishnu Menon <me@vishnumenon.com>
2017-05-01 18:25:18 -0400

    added some comments

--- clouds/cloudgen.py
+++ clouds/cloudgen.py
@@ -6,21 +6,28 @@ import json
 
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
+# Loop through memes
 for meme in ["rickroll"]: #["aesthetic", "emoji", "nessie", "obama", "ratings", "rickroll", "switcharoo", "wot"]:
     allText = ""
-    lines = 0
+
+    # Aggregate all bodies for the meme into one string
     for line in open(meme+".json"):
+        # Add body to variable
         allText += json.loads(line)["body"] + " "
-        lines += 1
-        if lines > 10000:
-            break
+
+    # Print status update
     print("Done reading")
+    # Load mask image
     mask = np.array(Image.open(meme+".png"))
+    # Create stopword set and add new stopwords for specific meme
     stopwords = set(STOPWORDS)
     stopwords.update(["youtube", "dQw4w9WgXcQ"])
 
+    # Load colormap for WC
     image_colors = ImageColorGenerator(mask)
 
+    # Generate word cloud for allText with specified color and shape masks
     wc = WordCloud(background_color=None, mode="RGBA", color_func=image_colors, max_words=1000, mask=mask, stopwords=stopwords)
     wc.generate(allText)
+    # Write to file
     wc.to_file(meme+"-cloud.png")
